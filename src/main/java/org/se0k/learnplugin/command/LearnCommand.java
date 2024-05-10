40f6260266b66472ac9c809884e4c8603a818841
S-E0K <rqt6238@naver.com>
2024-05-11 01:48:58 +0900

    완성

--- src/main/java/org/se0k/learnplugin/command/LearnCommand.java
+++ src/main/java/org/se0k/learnplugin/command/LearnCommand.java
@@ -1,6 +1,5 @@
 package org.se0k.learnplugin.command;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
@@ -9,6 +8,9 @@ import org.bukkit.command.CommandSender;
 import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class LearnCommand extends BukkitCommand {
 
@@ -19,50 +21,55 @@ public class LearnCommand extends BukkitCommand {
     @Override
     public boolean execute(@NotNull CommandSender sender, @NotNull String s, @NotNull String[] strings) {
 
-        if(sender instanceof Player player) {
+        if (!(sender instanceof Player player)) return false;
 
-            World world = player.getWorld();
-            Location playerLocation = player.getLocation();
+        randomTeleport(player);
 
-            Location location1 = new Location(world, -10000, 60, -10000);
-            Location location2 = new Location(world, 10000, 60, 10000);
-
-            double checkX = Math.floor(Math.random() * 20000);
-            double checkZ = Math.floor(Math.random() * 20000);
-
-
-            location1.setX(checkX - 10000);
-            location1.setZ(checkZ - 10000);
-
-
-
-
-
-
-            Block block = world.getBlockAt(playerLocation.getBlockX(), playerLocation.getBlockY() - 1, playerLocation.getBlockZ());
-            Material type = block.getType();
-
-            player.teleport(location1);
-
-
-
-
-//            for (double i = -60; i <= 320; i++) {
-//
-//
-//            }
-
-
-
-
-
-//            if (type == Material.AIR) {
-//                Bukkit.getLogger().warning("AIR");
-//            }
-//            else  Bukkit.getLogger().warning(type.toString());
-        }
-
-        return false;
+        return true;
     }
 
+    void randomTeleport(Player player) {
+        World world = player.getWorld();
+        List<Location> able = new ArrayList<>();
+
+        int checkX = (int) (Math.random() * 20001);
+        int checkZ = (int) (Math.random() * 20001);
+
+        for (int i = -64; i <= 320; i++) {
+            Location location = new Location(world, checkX - 10000 + 0.5, i, checkZ - 10000 + 0.5);
+
+            if (!isLiquid(location) && !isAIR(location)) {
+                location.setY(i + 1);
+                if (isAIR(location)) {
+                    location.setY(i + 2);
+                    if (isAIR(location)) {
+                        able.add(location);
+                    }
+                }
+            }
+        }
+
+        if (able.isEmpty()) {
+            player.sendMessage("다시 하세요");
+            return;
+        }
+
+        int size = able.size();
+        int checkY = (int) (Math.random() * size);
+
+        Location ableLocation = able.get(checkY);
+
+        player.teleport(ableLocation);
+
+    }
+
+    boolean isLiquid(Location under) {
+        Material getM = under.getBlock().getType();
+        return getM == Material.WATER || getM == Material.LAVA;
+    }
+
+    boolean isAIR(Location under) {
+        Material getM = under.getBlock().getType();
+        return getM == Material.AIR || getM == Material.CAVE_AIR || getM == Material.VOID_AIR;
+    }
 }
